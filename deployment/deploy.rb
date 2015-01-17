6f78626340cbd0cf8d8b77cc62fc7c977a54bf6a
Bruno Martin <brunoocto@gmail.com>
2015-01-17 23:54:40 +0800

    Web revision: 1421510080

--- deployment/deploy.rb
+++ deployment/deploy.rb
@@ -17,8 +17,11 @@ desc 'Init application'
 		on roles(:web), in: :sequence, wait: 1 do
 			#execute "cd #{release_path} && touch tata"
 			#execute "cd #{release_path} && chown -R apache:apache logs"
-			execute "su chown -R apache:apache #{release_path}/logs"
+			#execute "chown -R apache:apache #{release_path}/logs"
 			#execute "chown -R apache:apache #{release_path}/public"
+			as :root do
+				execute :chown, "-R apache:apache #{release_path}/logs"
+			end
 		end
 	end
 
