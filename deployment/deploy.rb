464f849942625891e8653ade950f66a69691ea0a
Bruno Martin <brunoocto@gmail.com>
2015-01-18 00:51:28 +0800

    Web revision: 1421513488

--- deployment/deploy.rb
+++ deployment/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 set :application, 'lincko'
-set :repo_url, 'ssh://root@192.168.137.180/var/www/lincko/git/slim.web'
+set :repo_url, 'ssh://deploy@192.168.137.180/var/www/lincko/git/slim.web'
 #ssh_options[:port] = 22
 set :deploy_to, '/var/www/lincko/slim.web'
 set :format, :pretty
@@ -9,18 +9,20 @@ set :pty, true
 set :linked_files, fetch(:linked_files, []).push('composer.lock')
 set :linked_dirs, fetch(:linked_dirs, []).push('logs', 'vendor', 'param')
 set :keep_releases, 5
-set :use_sudo, true
+#set :use_sudo, true
+
+# to avoid an error message at the first launch, create the composer.lock file manually, and copy the current content
+# "cd #{shared_path} && nano composer.lock"
 
 namespace :deploy do
 
-desc 'Init application'
+	desc 'Init application'
 	task :composer do
 		on roles(:web), in: :sequence, wait: 1 do
-			#execute "ln -s #{shared_path}/composer.lock #{release_path}/composer.lock"
-			# sudoer => give CHOWN privilege to deploy
-			execute "cd #{shared_path} && chown -R apache:apache logs"
-			execute "cd #{shared_path} && chown -R apache:apache public"
-			#execute "cd #{release_path} && composer update"
+			# On server, do only once manually the follow commands
+			# "cd #{shared_path} && chown -R apache:apache logs"
+			# "cd #{shared_path} && chown -R apache:apache public"
+			execute "cd #{release_path} && composer update"
 		end
 	end
 
