85004c78aeb1fd8d1f2478c400b5832f324e8244
Bruno Martin <brunoocto@gmail.com>
2015-01-18 00:29:37 +0800

    Web revision: 1421512177

--- deployment/deploy.rb
+++ deployment/deploy.rb
@@ -15,7 +15,11 @@ namespace :deploy do
 desc 'Init application'
 	task :composer do
 		on roles(:web), in: :sequence, wait: 1 do
-			execute "cd #{release_path} && composer update"
+			execute "ln -fs #{shared_path}/composer.lock #{release_path}/composer.lock"
+			# sudoer => give CHOWN privilege to deploy
+			execute "cd #{release_path} && chown -R apache:apache logs"
+			execute "cd #{release_path} && chown -R apache:apache public"
+			#execute "cd #{release_path} && composer update"
 		end
 	end
 
