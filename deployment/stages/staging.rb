7b5e1ee67ce265e1b93df45e96024ff89dd487b9
Bruno Martin <brunoocto@gmail.com>
2015-01-18 10:27:51 +0800

    Web revision: 1421548071

--- deployment/stages/staging.rb
+++ deployment/stages/staging.rb
@@ -1,4 +1,4 @@
 set :repo_url, 'ssh://deploy@192.168.137.180:2243/var/www/lincko/git/slim.web'
 set :deploy_to, '/var/www/lincko/slim.web'
-role :web, %w{deploy@192.168.137.180}
+role :web, %w{deploy@192.168.137.180:2243}
 server '192.168.137.180', user: 'deploy', port: 2243 , roles: %w{web}, my_property: :my_value
