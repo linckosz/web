9a08fc21ae797c2065ec5c9810204b70e8dd829e
Bruno Martin <brunoocto@gmail.com>
2015-01-23 09:03:32 +0800

    Web revision: 1421975012

--- deployment/stages/staging.rb
+++
@@ -1,5 +0,0 @@
-set :root_path, "/glusterfs/.lincko.co"
-set :repo_url, "ssh://deploy@lincko.co:2244#{fetch(:root_path)}/deploy/share/git/slim.web"
-set :deploy_to, "#{fetch(:root_path)}/www/share/slim.web"
-role :web, %w{deploy@lincko.co}
-#server 'lincko.co', user: 'deploy', roles: %w{web}, port: 2244, my_property: :my_value
