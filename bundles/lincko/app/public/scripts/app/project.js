47adfbb393493afbb78c6faf992b11873629b59a
Bruno Martin <brunoocto@gmail.com>
2015-03-15 13:51:41 +0800

    Web revision: 1426398701

--- bundles/lincko/app/public/scripts/app/project.js
+++ bundles/lincko/app/public/scripts/app/project.js
@@ -1,5 +1,51 @@
+function app_project_quick_upload_display(Elem, show) {
+	var Obj_div = $('#app_project_quick_upload');
+	var Obj_img = $('#app_project_quick_upload > div > img');
+	var timing = 200;
+
+
+	if(typeof show === 'undefined') { show = true; }
+	if(Elem !== null) {
+		if(Elem.length > 0){
+			Obj_div.css('margin-bottom', $(window).height() - Elem.offset().top);
+		}
+	}
+	if(Obj_div.is(':visible')){
+		//Obj.velocity("transition.slideDownOut", { duration: 500 });
+		$.Velocity.RunSequence([
+			{ e: Obj_div, p: "transition.slideDownOut", o: { duration: timing, sequenceQueue: false } },
+			{ e: Obj_img, p: "transition.expandOut", o: { duration: timing, sequenceQueue: false } }
+		]);
+	} else if(Obj_div.is(':hidden') && show){
+		//Obj.velocity("transition.slideUpIn", { duration: 500 });
+		$.Velocity.RunSequence([
+			{ e: Obj_div, p: "transition.slideUpIn", o: { duration: timing, sequenceQueue: false } },
+			{ e: Obj_img, p: "transition.expandIn", o: { duration: timing, sequenceQueue: false } }
+		]);
+	}
+
+}
+
 $('#app_project_close').click(function(){
 	if(typeof app_application !== 'undefined'){
 		app_application.move('project');
 	}
+});
+
+$('#app_project_quick_access_upload').click(function(){
+	app_project_quick_upload_display($(this));
+});
+
+$('html').click(function(){
+	if($('#app_project_quick_upload').is(':visible') && !app_project_quick_upload){
+		app_project_quick_upload_display(null, false);
+	}
+});
+
+var app_project_quick_upload = false;
+$('#app_project_quick_upload').hover(function(){
+	app_project_quick_upload = true;
+});
+$('#app_project_quick_upload').mouseleave(function(){
+	app_project_quick_upload = false;
 });
