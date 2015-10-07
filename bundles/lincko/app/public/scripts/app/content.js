a453292e9a4bfcac391dd2e8d4ed6c358e6ec3a4
Bruno Martin <brunoocto@gmail.com>
2015-10-07 11:02:36 +0800

    Web revision: 1444186956

--- bundles/lincko/app/public/scripts/app/content.js
+++ bundles/lincko/app/public/scripts/app/content.js
@@ -17,6 +17,7 @@ function app_content_menu_position() {
 		}
 	});
 	msg = $('#app_content_menu').height()+" / "+$('#app_content_menu').width()+" / ";
+	$('#app_content_menu').css("z-index", 100);
 	base_show_error(msg);
 }
 function app_content_menu_position_old() {
