aa8fe07d10e5523c4e77058430779b6277712172
Bruno Martin <brunoocto@gmail.com>
2016-01-13 17:51:43 +0800

    Web revision: 1452678703

--- bundles/lincko/wrapper/public/scripts/wrapper.js
+++ bundles/lincko/wrapper/public/scripts/wrapper.js
@@ -364,7 +364,7 @@ var wrapper_IScroll_options = {
 
 if(supportsTouch){
 	wrapper_IScroll_options.click = false;
-	interactiveScrollbars: false,
+	wrapper_IScroll_options.interactiveScrollbars = false;
 }
 
 var wrapper_IScroll_options_new = {};
