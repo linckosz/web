ef3303b89cc7cfd245b893b47ba2a409f548b29f
Apache <apache@front.lincko.net>
2016-02-14 16:22:51 +0800

    Web revision: 1455438171

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ssh-add /root/.ssh/id_rsa
+ssh-add /apache_ssh/id_rsa
 ssh-add -l
 #git rm -r --cached .
 git add .
 git commit -a -m "Web revision: $(date +'%s')"
 git push $1 master
-cap $1 deploy --trace
+#cap $1 deploy --trace
