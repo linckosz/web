5bd0d5029c75e85d261f362dde01f9229a8903ca
Apache <apache@front.lincko.net>
2016-02-14 16:00:20 +0800

    Web revision: 1455436820

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 ssh-add /root/.ssh/id_rsa
 ssh-add -l
-git config --global user.name "Bruno Martin"
-git config --global user.email "brunoocto@gmail.com"
 #git rm -r --cached .
 git add .
+git config --global user.name "Bruno Martin"
+git config --global user.email "brunoocto@gmail.com"
 git commit -a -m "Web revision: $(date +'%s')"
 git push $1 master
 cap $1 deploy --trace
