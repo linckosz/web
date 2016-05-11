dc35816d7e9b46b053bda3b9377d9bc1ad2c947c
Bruno Martin <bruno.martin@lincko.com>
2016-05-11 08:29:52 +0800

    WEB 20160511 02 Bruno - Bug fix

--- bundles/lincko/app/public/scripts/generic/storage.js
+++ bundles/lincko/app/public/scripts/generic/storage.js
@@ -1,5 +1,4 @@
 var storage_first_request = true; //Help to launch getSchema within getLatest only once at the beginning to insure nothing is missing
-var storage_call_missing = false;
 /* PRIVATE METHOD */
 /*
 	NOTE: Do not add this callback to wrapper_sendAction, because wrapper_ajax already launches it internally
@@ -205,7 +204,6 @@ Lincko.storage.update = function(partial, info){
 /* PRIVATE METHOD */
 Lincko.storage.schema = function(schema){
 	var update = false;
-	var calling_missing = false;
 	var missing = {};
 
 	storage_first_request = false; //No need to launch firstLatest()
