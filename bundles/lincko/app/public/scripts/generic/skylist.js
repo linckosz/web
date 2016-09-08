10df9ec4e5ead5dd39b69875b072ca62a2d5618d
Skyler Park <skyler.park@lincko.com>
2016-09-08 14:07:35 +0800

    SKY: fixed @search bug

--- bundles/lincko/app/public/scripts/generic/skylist.js
+++ bundles/lincko/app/public/scripts/generic/skylist.js
@@ -651,7 +651,10 @@ skylist.prototype.filter_by_search = function(items, filter){
 					for( var k=0; k < userid_array.length; k++){
 						userid = userid_array[k];
 						if( that.list_type == 'tasks' ){
-							if( userid in item['_users'] && item['_users'][userid]['in_charge'] ){
+							if(!item['_users']){ //not assigned
+								break;
+							}
+							else if( userid in item['_users'] && item['_users'][userid]['in_charge'] ){
 								push = true;
 								break;
 							}
