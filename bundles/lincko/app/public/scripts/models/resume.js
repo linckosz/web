140ef843f2ab60df8970647f35c3836dd1055c16
Bruno Martin <bruno.martin@lincko.com>
2016-11-01 08:29:38 +0800

    WEB 20161101 01 bruno - Add onboarding process inside resume.js - Change color of Skycard

--- bundles/lincko/app/public/scripts/models/resume.js
+++ bundles/lincko/app/public/scripts/models/resume.js
@@ -8,187 +8,272 @@ var app_models_resume_format_sentence = function(comments_id, type) {
 
 	var comment = Lincko.storage.get('comments', comments_id, 'comment');
 	var sentence = '';
-	var error = false;
+	var format = false; //Display it as a text by default
 	var base = 0;
 	var uid = 0;
-	if(!comment){
-		error = true;
-	} else {
+	var list = {};
+	if(comment){
 		try {
 			var temp = JSON.parse(comment);
-			if(temp[0]){ //By default we force to display team in priority
+			if($.type(temp) != 'object'){
+				format = 'quote';
+			} else if(temp[0]){ //By default we force to display team in priority
 				uid = 0;
+				list = temp[uid];
+				format = 'resume';
 			} else if(temp[wrapper_localstorage.uid]){
 				uid = wrapper_localstorage.uid;
-			} else {
-				return false;
-			}
-			if($.type(temp) != 'object' || !temp[uid]){
-				error = true;
-				if($.type(temp) == 'number'){
-					base = temp;
+				list = temp[uid];
+				format = 'resume';
+			} else if(temp['ob']){
+				if($.type(temp['ob']) == 'object'){
+					for(var i in temp['ob']){
+						format = 'onboarding';
+						sentence = i;
+						list = temp['ob'][i];
+						break; //Just grab the first ID
+					}
 				}
+			} else if(temp['quote']){
+				format = 'quote';
 			} else {
-				var list = temp[uid];
-				if(list[100]){
-					base = 100; //Daily
-				} else if(list[700]){
-					base = 700; //Weekly
-				} else {
-					error = true;
-				}
+				format = false;
 			}
+
 		} catch(err) {
-			error = true;
+			format = false;
 		}
+	} else {
+		return false;
 	}
-	if(error){
-		if(base == 700){
-			return $('<span>')
-				.addClass('unselectable')
-				.html(wrapper_to_html(
-					Lincko.Translation.get('app', 3817, 'html') //There was no activity last week.
-				));
-		} else if(base == 100){
-			return $('<span>')
-				.addClass('unselectable')
-				.html(wrapper_to_html(
-					Lincko.Translation.get('app', 3811, 'html') //There was no activity yesterday.
-				));
+
+	//No activity comments
+	if(comment==100){
+		base = 100; //Daily
+		return $('<span>')
+			.addClass('unselectable')
+			.html(wrapper_to_html(
+				Lincko.Translation.get('app', 3811, 'html') //There was no activity yesterday.
+			));
+	} else if(comment==700){
+		return $('<span>')
+			.addClass('unselectable')
+			.html(wrapper_to_html(
+				Lincko.Translation.get('app', 3817, 'html') //There was no activity last week.
+			));
+	}
+
+	if(format == 'resume'){
+
+		if(list[100]){
+			base = 100; //Daily
+		} else if(list[700]){
+			base = 700; //Weekly
+		}
+
+		var data = {};
+		for (var i = base; i < base+100; i++) {
+			data[i] = '0';
+			if(list[i]){
+				if(typeof list[i] == 'object'){
+					data[i] = ''+list[i].length;
+				} else if(i==base+8){
+					data[i] = ''+Lincko.storage.get('users', list[i], 'username');
+				} else {
+					data[i] = ''+list[i];
+				}
+			}
+		}
+		
+		if(uid==0){
+			if(base==100){
+				if(type==1){
+					sentence = Lincko.Translation.get('app', 3801, 'pure', data);
+					if(typeof data[108] != 'undefined' && data[108]!=0){
+						sentence = sentence + ' ' + Lincko.Translation.get('app', 3802, 'pure', data);
+					}
+				}
+			} else if(base==700){
+				if(type==1){
+					sentence = Lincko.Translation.get('app', 3803, 'pure', data);
+					if(typeof data[708] != 'undefined' && data[708]!=0){
+						sentence = sentence + ' ' + Lincko.Translation.get('app', 3804, 'pure', data);
+					}
+				}
+			}
 		} else {
-			//A random quote (will be the same for the same comment ID)
-			var random_quote = parseInt(comments_id, 10);
-			var random_quote = 9901 + random_quote - (46 * Math.floor( random_quote / 46));
-
-			return $('<span>')
-				.addClass('unselectable')
-				.html(wrapper_to_html(
-					Lincko.Translation.get('app', random_quote, 'html') //“Knowing trees, I understand the meaning of patience. Knowing grass, I can appreciate persistence.” – Hal Borland
-				));
+			data['username'] = Lincko.storage.get('users', uid, 'username');
+			if(base==100){
+				if(type==1){
+					data['motivation'] = '';
+					if(typeof data[132] != 'undefined' && data[132]>0){ //Done more than last week
+						data['motivation'] = Lincko.Translation.get('app', 3808, 'pure'); //That's more than yesterday. Great work!
+					}
+					sentence = Lincko.Translation.get('app', 3807, 'pure', data); //Hi [{username}]: You completed <span find="101">[{101}] task(s)</span> today![{motivation}]You have <span find="130">[{130}] task(s)</span> due tomorrow. You currently have <span find="105">[{105}] overdue task(s)</span>.
+				}
+			} else if(base==700){
+				if(type==1){
+					data['whatweek'] = '';
+					if(typeof data[701] != 'undefined' && data[701]>=2){ //At least 2 completed tasks during the week
+						data['whatweek'] = Lincko.Translation.get('app', 3812, 'pure'); //What a week!
+					}
+					data['motivation'] = '';
+					if(typeof data[732] != 'undefined' && data[732]>0){
+						data['motivation'] = Lincko.Translation.get('app', 3810, 'pure'); //That's more than last week! You're really making progress.
+					}
+					sentence = Lincko.Translation.get('app', 3809, 'pure', data); //Hi [{username}]: [{whatweek}]You completed <span find="701">[{701}] task(s)</span> this week.[{motivation}]You have <span find="730">[{730}] task(s)</span> due next week... but it's the weekend so make sure to take a break. You currently have <span find="705">[{705}] overdue task
+				}
+			}
 		}
-	}
 
-	var data = {};
-	for (var i = base; i < base+100; i++) {
-		data[i] = '0';
-		if(list[i]){
+		if(uid==0){
+			sentence = sentence + '<div find="result" link="" class="ellipsis"></div>';
+			var span = $('<span>').html(sentence);
+		} else {
+			var span = $('<span>').addClass('app_models_resume_individual_span');
+			var cell_shhh = $('<span>').addClass('app_models_resume_individual_shhh').html("["+Lincko.Translation.get('app', 3805, 'pure')+"]"); //Shhh
+			var cell_info = $('<span>');
+
+			var private = $('<div>').addClass('app_models_resume_individual_private ellipsis').html(Lincko.Translation.get('app', 3806, 'pure')); //Private Message - Only you can see this.
+			cell_info.append(private);
+			cell_info.append(sentence);
+			span.append(cell_shhh);
+			span.append(cell_info);
+			span.append('<div find="result" link="" class="ellipsis"></div>');
+		}
+		span.addClass('app_models_resume_span unselectable');
+		span.attr('find', 'anchor_'+comments_id);
+		var result = {};
+		//Attach event click
+		//Task list
+		links = {
+			
+			101: ['tasks', 'app_models_resume_tasks', ],
+			701: ['tasks', 'app_models_resume_tasks', ],
+
+			102: ['tasks', 'app_models_resume_tasks', ],
+			702: ['tasks', 'app_models_resume_tasks', ],
+			
+			103: ['tasks', 'app_models_resume_tasks', ],
+			703: ['tasks', 'app_models_resume_tasks', ],
+
+			104: ['tasks', 'app_models_resume_tasks', ],
+			704: ['tasks', 'app_models_resume_tasks', ],
+			
+			105: ['tasks', 'app_models_resume_tasks_overdue', ],
+			705: ['tasks', 'app_models_resume_tasks_overdue', ],
+
+			111: ['notes', 'app_models_resume_notes', ],
+			711: ['notes', 'app_models_resume_notes', ],
+
+			121: ['files', 'app_models_resume_files', ],
+			721: ['files', 'app_models_resume_files', ],
+
+			130: ['tasks', 'app_models_resume_tasks', ],
+			730: ['tasks', 'app_models_resume_tasks', ],
+
+			131: ['tasks', 'app_models_resume_tasks', ],
+			731: ['tasks', 'app_models_resume_tasks', ],
+
+		};
+
+		for(var i in links){
 			if(typeof list[i] == 'object'){
-				data[i] = ''+list[i].length;
-			} else if(i==base+8){
-				data[i] = ''+Lincko.storage.get('users', list[i], 'username');
-			} else {
-				data[i] = ''+list[i];
+				if(list[i].length<=0){
+					continue;
+				}
+				result = {};
+				result[links[i][0]] = list[i];
+				span.find("[find="+i+"]")
+				.click([span, result, links[i][1], i, comments_id], function(event){
+					app_models_resume_listup(event.data[0], event.data[1], event.data[2], event.data[3], event.data[4]);
+				})
+				.addClass(links[i][1]);
 			}
 		}
-	}
-	if(uid==0){
-		if(base==100){
-			if(type==1){
-				sentence = Lincko.Translation.get('app', 3801, 'pure', data);
-				if(typeof data[108] != 'undefined' && data[108]!=0){
-					sentence = sentence + ' ' + Lincko.Translation.get('app', 3802, 'pure', data);
+
+		return span;
+
+	} else if(format == 'onboarding'){
+		var text = wrapper_to_html(Lincko.Translation.get('app', sentence, 'html'));
+		var span = $('<span>').html(text);
+		var span_arr = [];
+		var j = 0;
+		var next = false;
+		if($.type(list) == 'object'){
+			for(var i in list){
+				var answer = list[i];
+				var next = answer[1];
+				text = wrapper_to_html(Lincko.Translation.get('app', i, 'html'));
+				span_arr[j] = $('<span>').html(text).addClass('app_models_resume_onboarding_answer');
+				if(answer[0]=='action'){
+					span_arr[j].click([comments_id, answer], function(event){
+						var current = event.data[0];
+						var answer = event.data[1];
+						var param = [];
+						for(var k in answer){
+							if(k>=2){
+								param.push(answer[k]);
+							}
+						}
+						if(param.length>0){
+							//This function must call "app_models_resume_onboarding_continue(current, next)" once the action is completed
+							app_models_sky_fn(current, next, param); //toto
+						} else {
+							app_models_resume_onboarding_continue(current, next);
+						}
+					});
+					//span_arr[j].click();
+				} else {
+					span_arr[j].click([comments_id, next], function(event){
+						var current = event.data[0];
+						var next = event.data[1];
+						app_models_resume_onboarding_continue(current, next);
+					});
+					//span_arr[j].click();
 				}
-			}
-		} else if(base==700){
-			if(type==1){
-				sentence = Lincko.Translation.get('app', 3803, 'pure', data);
-				if(typeof data[708] != 'undefined' && data[708]!=0){
-					sentence = sentence + ' ' + Lincko.Translation.get('app', 3804, 'pure', data);
-				}
+				j = span_arr.length;
 			}
 		}
-	} else {
-		data['username'] = Lincko.storage.get('users', uid, 'username');
-		if(base==100){
-			if(type==1){
-				data['motivation'] = '';
-				if(typeof data[132] != 'undefined' && data[132]>0){ //Done more than last week
-					data['motivation'] = Lincko.Translation.get('app', 3808, 'pure'); //That's more than yesterday. Great work!
-				}
-				sentence = Lincko.Translation.get('app', 3807, 'pure', data); //Hi [{username}]: You completed <span find="101">[{101}] task(s)</span> today![{motivation}]You have <span find="130">[{130}] task(s)</span> due tomorrow. You currently have <span find="105">[{105}] overdue task(s)</span>.
-			}
-		} else if(base==700){
-			if(type==1){
-				data['whatweek'] = '';
-				if(typeof data[701] != 'undefined' && data[701]>=2){ //At least 2 completed tasks during the week
-					data['whatweek'] = Lincko.Translation.get('app', 3812, 'pure'); //What a week!
-				}
-				data['motivation'] = '';
-				if(typeof data[732] != 'undefined' && data[732]>0){
-					data['motivation'] = Lincko.Translation.get('app', 3810, 'pure'); //That's more than last week! You're really making progress.
-				}
-				sentence = Lincko.Translation.get('app', 3809, 'pure', data); //Hi [{username}]: [{whatweek}]You completed <span find="701">[{701}] task(s)</span> this week.[{motivation}]You have <span find="730">[{730}] task(s)</span> due next week... but it's the weekend so make sure to take a break. You currently have <span find="705">[{705}] overdue task
-			}
+		if(span_arr.length>0){
+			//This function should be internally launched once the DOM of the chat is finished to be drawn, otherwise we may not be able to find the position where to insert the answers
+			app_models_even_fn(comments_id, span_arr); //toto
 		}
+		return span;
+	} else if(format == 'quote'){
+		//A random quote (will be the same for the same comment ID)
+		var random_quote = parseInt(comments_id, 10);
+		var random_quote = 9901 + random_quote - (46 * Math.floor( random_quote / 46));
+
+		return $('<span>')
+			.addClass('unselectable')
+			.html(wrapper_to_html(
+				Lincko.Translation.get('app', random_quote, 'html') //“Knowing trees, I understand the meaning of patience. Knowing grass, I can appreciate persistence.” – Hal Borland
+			));
+	} else if(!format){
+		return $('<span>').html(wrapper_to_html(comment));
 	}
 
-	if(uid==0){
-		sentence = sentence + '<div find="result" link="" class="ellipsis"></div>';
-		var span = $('<span>').html(sentence);
-	} else {
-		var span = $('<span>').addClass('app_models_resume_individual_span');
-		var cell_shhh = $('<span>').addClass('app_models_resume_individual_shhh').html("["+Lincko.Translation.get('app', 3805, 'pure')+"]"); //Shhh
-		var cell_info = $('<span>');
+	return false;
 
-		var private = $('<div>').addClass('app_models_resume_individual_private ellipsis').html(Lincko.Translation.get('app', 3806, 'pure')); //Private Message - Only you can see this.
-		cell_info.append(private);
-		cell_info.append(sentence);
-		span.append(cell_shhh);
-		span.append(cell_info);
-		span.append('<div find="result" link="" class="ellipsis"></div>');
-	}
-	span.addClass('app_models_resume_span unselectable');
-	span.attr('find', 'anchor_'+comments_id);
-	var result = {};
-	//Attach event click
-	//Task list
-	links = {
-		
-		101: ['tasks', 'app_models_resume_tasks', ],
-		701: ['tasks', 'app_models_resume_tasks', ],
+};
 
-		102: ['tasks', 'app_models_resume_tasks', ],
-		702: ['tasks', 'app_models_resume_tasks', ],
-		
-		103: ['tasks', 'app_models_resume_tasks', ],
-		703: ['tasks', 'app_models_resume_tasks', ],
+var app_models_sky_fn = function(current, next, param){
+	console.log('app_models_sky_fn: '+current+' => '+next);
+	console.log(param);
+};
 
-		104: ['tasks', 'app_models_resume_tasks', ],
-		704: ['tasks', 'app_models_resume_tasks', ],
-		
-		105: ['tasks', 'app_models_resume_tasks_overdue', ],
-		705: ['tasks', 'app_models_resume_tasks_overdue', ],
+var app_models_even_fn = function(current, span_arr){
+	console.log('app_models_even_fn: '+current);
+	console.log(span_arr);
+};
 
-		111: ['notes', 'app_models_resume_notes', ],
-		711: ['notes', 'app_models_resume_notes', ],
-
-		121: ['files', 'app_models_resume_files', ],
-		721: ['files', 'app_models_resume_files', ],
-
-		130: ['tasks', 'app_models_resume_tasks', ],
-		730: ['tasks', 'app_models_resume_tasks', ],
-
-		131: ['tasks', 'app_models_resume_tasks', ],
-		731: ['tasks', 'app_models_resume_tasks', ],
-
+var app_models_resume_onboarding_continue = function(current, next){
+	var data = {
+		current: current,
+		next: next,
 	};
-
-	for(var i in links){
-		if(typeof list[i] == 'object'){
-			if(list[i].length<=0){
-				continue;
-			}
-			result = {};
-			result[links[i][0]] = list[i];
-			span.find("[find="+i+"]")
-			.click([span, result, links[i][1], i, comments_id], function(event){
-				app_models_resume_listup(event.data[0], event.data[1], event.data[2], event.data[3], event.data[4]);
-			})
-			.addClass(links[i][1]);
-		}
-	}
-
-	return span;
+	wrapper_sendAction(data, 'post', 'onboarding/next');
 };
 
 var app_models_resume_listup = function(Elem, list, color, link, comments_id){
