a4b8c65040c677270bfc55b1bdfbdbde54db57fa
Bruno Martin <bruno.martin@lincko.com>
2016-12-12 17:57:06 +0800

    WEB 20161212 06 bruno - Wechat lib update

--- bundles/lincko/wrapper/controllers/integration/ControllerWechat.php
+++ bundles/lincko/wrapper/controllers/integration/ControllerWechat.php
@@ -7,20 +7,33 @@ use \libs\Controller;
 class ControllerWechat extends Controller {
 
 	protected $app = NULL;
-	protected $data = NULL;
+	protected $get = NULL;
 
 	public function __construct(){
 		$app = $this->app = \Slim\Slim::getInstance();
-		$this->data = json_decode($app->request->getBody());
+		$this->get = $app->request->get();
 		return true;
 	}
 
-	public function token_get($var=false){
+	public function token_get(){
 		$app = $this->app;
-		$data = $app->request->get();
-		\libs\Watch::php($data, '$data', __FILE__, false, false, true);
-		if(isset($data['code'])){
-			$url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$app->lincko->integration->wechat['appid'].'&secret='.$app->lincko->integration->wechat['secretapp'].'&code='.$data['code'].'&grant_type=authorization_code';
+		$this->curl('authorization_code');
+		$app->lincko->data['link_reset'] = true;
+		$app->router->getNamedRoute('home')->dispatch();
+	}
+
+	public function curl($grant_type=false){
+		$app = $this->app;
+		$get = $this->get;
+		\libs\Watch::php($get, '$get', __FILE__, false, false, true);
+		$resut = false;
+		if(isset($get['code'])){
+			
+			if($grant_type=='authorization_code'){
+				$url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$app->lincko->integration->wechat['appid'].'&secret='.$app->lincko->integration->wechat['secretapp'].'&code='.$get['code'].'&grant_type=authorization_code';
+			} else {
+				return false;
+			}
 			$timeout = 8;
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $url); //Port used is 10443 only
@@ -37,9 +50,8 @@ class ControllerWechat extends Controller {
 			curl_setopt($ch, CURLOPT_STDERR, $verbose);
 
 			if($result = curl_exec($ch)){
-				\libs\Watch::php($result, '$result', __FILE__, false, false, true);
+				\libs\Watch::php(json_decode($result), '$result', __FILE__, false, false, true);
 			} else {
-				//echo "cURL error!\n";
 				\libs\Watch::php(curl_getinfo($ch), '$ch', __FILE__, false, false, true);
 				$error = '['.curl_errno($ch)."] => ".htmlspecialchars(curl_error($ch));
 				\libs\Watch::php($error, '$error', __FILE__, false, false, true);
@@ -49,9 +61,9 @@ class ControllerWechat extends Controller {
 			}
 
 			@curl_close($ch);
-			$app->lincko->data['link_reset'] = true;
-			$app->router->getNamedRoute('home')->dispatch();
+			
 		}
+		return $result;
 	}
 
 }
