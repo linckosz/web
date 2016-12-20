eb0d1f337c62cc22456b581720e63ad5328e8e0b
EvanLin018 <evan.lin@lincko.com>
2016-12-20 22:05:15 +0800

    filemanager upload file icon

--- bundles/lincko/app/public/scripts/submenu/filemanager.js
+++ bundles/lincko/app/public/scripts/submenu/filemanager.js
@@ -20,7 +20,15 @@ submenu_list['filemanager'] = {
 		'hide': true,
 		"class": "base_pointer",
 		"action": function(Elem, subm) {
-			
+			$.each(subm.param.upload_flag, function(key,val){
+				if(!val)
+				{
+					var e;
+					app_upload_files.lincko_files[key].lincko_status = 'deleted';
+					$('#app_upload_fileupload').fileupload('option').destroy(e, app_upload_files.lincko_files[key]);
+				}
+			});
+
 		},
 	},
 	"content": {
@@ -72,7 +80,6 @@ Submenu.prototype.AddFilemanagerContent = function() {
 	var item_height = item_width;
 	target.find(".file_manager_file_item").height(item_height);
 	target.find(".file_manager_file_item").css("line-height",Math.floor(item_height)+"px");
-	target.find(".icon_file_manager_file_add").css("font-size",Math.floor(item_width/3)+"px");
 
 	submenu_content.append(target);
 
@@ -84,6 +91,7 @@ Submenu.prototype.AddFilemanagerContent = function() {
 		app_upload_open_files(upload_type, upload_pid,false,false,upload_param);
 	});
 
+	that.param.upload_flag = {};
 
 	var file_refresh =  function(upload_type,upload_pid,upload_param,upload_flag,target,item_width,item_height,border_size)
 	{
@@ -103,7 +111,7 @@ Submenu.prototype.AddFilemanagerContent = function() {
 
 					item.height(item_height);
 					item.css("line-height",Math.floor(item_height)+"px");
-					item.find(".icon_file_manager_file_add").css("font-size",Math.floor(item_height/3)+"px");
+					
 				}
 				var preview = null;
 				try{
@@ -113,65 +121,119 @@ Submenu.prototype.AddFilemanagerContent = function() {
 				{
 
 				}
-				if(preview == null || preview == ''){
 
+
+				if(typeof item.attr("has_border") === "undefined")
+				{
+					
+				}	
+
+				if(preview == null || preview == ''){
+					item.find("[find=file_content_ico]").removeClass("display_none");
+					item.find("[find=file_content]").addClass("display_none");
+					item.find("[find=file_content_ico]")
+					.addClass(app_models_fileType.getClass(app_models_fileType.getExt(files[i].lincko_name)));
+					if(typeof item.attr("has_border") === "undefined")
+					{
+						item.find(".file_manager_file_content_wrapper").css("width",item_width+"px");
+						item.find(".file_manager_file_content_wrapper").css("height",item_height+"px");
+						item.find("[find=file_content_ico]").removeClass("display_none");
+						item.find("[find=file_content]").addClass("display_none");
+
+						item.find(".file_manager_file_item_border").css("width",(item_width - border_size * 2)+"px");
+						item.find(".file_manager_file_item_border").css("height",(item_height - border_size * 2)+"px");
+						item.find(".file_manager_file_item_border").css("top",-item_height);
+						item.find(".file_manager_file_item_border").css("border-width",border_size+"px");
+						item.find(".file_manager_file_item_border").attr("file-index",files[i]["lincko_files_index"]);
+
+						item.find("[find=file_item_border]").on("click", function(event) {
+							var index = $(this).attr("file-index");
+							if($(this).css("border-width")=="0px")
+							{
+								that.param.upload_flag[index]=true;
+								$(this).css("border-width",border_size+"px");
+							}
+							else
+							{
+								that.param.upload_flag[index]=false;
+								$(this).css("border-width","0px");
+							}
+						});
+
+						item.attr("has_border",true);
+					}	
 				}
 				else
 				{
+					item.find("[find=file_content_ico]").addClass("display_none");
+					item.find("[find=file_content]").removeClass("display_none");
+
 					item.find("[find=file_content]").attr("src",preview);
 					var org_img_width = preview.width;//item.find("[find=file_content]").eq(0).width();
 					var org_img_height = preview.height;//item.find("[find=file_content]").eq(0).height();
-					if(typeof item.attr("resize") == "undefined" && org_img_width != 0 && org_img_height != 0)
+					org_img_width = item.find("[find=file_content]").eq(0).width();
+					org_img_height = item.find("[find=file_content]").eq(0).height();
+
+
+					var img_width;
+					var img_height;
+
+					if(org_img_width  > org_img_height){
+						var img_height = item_height;
+						var img_width = img_height / org_img_height * org_img_width ;
+						item.find("[find=file_content]").eq(0).css("height",Math.floor(img_height)+"px");
+					}
+					else{
+						var img_width = item_width;
+						var img_height = img_width / org_img_width * org_img_height;
+						item.find("[find=file_content]").eq(0).css("width",Math.floor(img_width)+"px");
+					}
+
+					if(typeof item.attr("has_border") === "undefined")
 					{
-						org_img_width = item.find("[find=file_content]").eq(0).width();
-						org_img_height = item.find("[find=file_content]").eq(0).height();
+						item.find(".file_manager_file_content_wrapper").css("width",item_width+"px");
+						item.find(".file_manager_file_content_wrapper").css("height",item_height+"px");
 
-						if(org_img_width  > org_img_height){
-							var img_height = item_height;
-							var img_width = img_height / org_img_height * org_img_width ;
-							item.find("[find=file_content]").eq(0).css("height",Math.floor(img_height)+"px");
+						item.find(".file_manager_file_item_border").css("width",(item_width - border_size * 2)+"px");
+						item.find(".file_manager_file_item_border").css("height",(item_height - border_size * 2)+"px");
+						item.find(".file_manager_file_item_border").css("top",-item_height);
+						item.find(".file_manager_file_item_border").css("border-width",border_size+"px");
+						item.find(".file_manager_file_item_border").attr("file-index",files[i]["lincko_files_index"]);
 
-							item.find(".file_manager_file_content_wrapper").css("width",item_width+"px");
-							item.find(".file_manager_file_content_wrapper").css("height",item_height+"px");
+						item.find("[find=file_item_border]").on("click", function(event) {
+							var index = $(this).attr("file-index");
+							if($(this).css("border-width")=="0px")
+							{
+								that.param.upload_flag[index]=true;
+								$(this).css("border-width",border_size+"px");
+							}
+							else
+							{
+								that.param.upload_flag[index]=false;
+								$(this).css("border-width","0px");
+							}
+						});
 
-							item.find(".file_manager_file_item_border").css("width",(item_width - border_size * 2)+"px");
-							item.find(".file_manager_file_item_border").css("height",(item_height - border_size * 2)+"px");
-							item.find(".file_manager_file_item_border").css("top",-item_height);
-							item.find(".file_manager_file_item_border").css("border-width",border_size+"px");
-							item.find(".file_manager_file_item_border").attr("file-index",files[i]["lincko_files_index"]);
+						item.attr("has_border",true);
+					}
 
-							item.find("[find=file_content]").eq(0).css("margin-left",Math.floor((item_width - img_width)/2)+"px");
-						}
-						else{
-							var img_width = item_width;
-							var img_height = img_width / org_img_width * org_img_height;
-							item.find("[find=file_content]").eq(0).css("width",Math.floor(img_width)+"px");
+					var index = files[i]["lincko_files_index"];
 
-							item.find(".file_manager_file_content_wrapper").css("width",item_width+"px");
-							item.find(".file_manager_file_content_wrapper").css("height",item_height+"px");
-
-							item.find(".file_manager_file_item_border").css("width",(item_width - border_size * 2)+"px");
-							item.find(".file_manager_file_item_border").css("height",(item_height - border_size * 2)+"px");
-							item.find(".file_manager_file_item_border").css("top",-item_height);
-							item.find(".file_manager_file_item_border").css("border-width",border_size+"px");
-							item.find(".file_manager_file_item_border").attr("file-index",files[i]["lincko_files_index"]);
-
-							item.find("[find=file_content]").css("margin-top",Math.floor((item_width - img_width)/2)+"px");
-						}
-						if(upload_flag[files[i]["lincko_files_index"]] == "undefined")
-						{
-							upload_flag[files[i]["lincko_files_index"] = true;
-						}
-						item.attr("resize",true);
+					if(typeof upload_flag[index] === "undefined")
+					{
+						upload_flag[index] = true;
 					}
 				}
+
+					
+				
 				
 			}
 		}
 	}
 
 
-	that.param.upload_flag = {};
+	
 
 	setTimeout(function(){
 		file_refresh(that.param.upload_type,that.param.upload_pid,that.param.upload_param,that.param.upload_flag,target,item_width,item_height,border_size);
@@ -189,23 +251,11 @@ Submenu.prototype.AddFilemanagerContent = function() {
 		var item_width = this.action_param[5];
 		var item_height = this.action_param[6];
 
-		file_refresh(upload_type,upload_pid,upload_param,target,item_width,item_height,border_size);
+		file_refresh(upload_type,upload_pid,upload_param,upload_param,target,item_width,item_height,border_size);
 		wrapper_IScroll_refresh();
 		wrapper_IScroll();
 
 	},[that.param.upload_type,that.param.upload_pid,that.param.upload_param,that.param.upload_flag,target,item_width,item_height]);
-
-	
-	// $("body").on("touchend", '.file_manager_file_item_border', function(event) {
-	// 	if($(this).css("border-width")=="0px")
-	// 	{
-	// 		$(this).css("border-width",border_size+"px");
-	// 	}
-	// 	else
-	// 	{
-	// 		$(this).css("border-width","0px");
-	// 	}
-	// });
 
 	delete submenu_wrapper;
 	return true;
