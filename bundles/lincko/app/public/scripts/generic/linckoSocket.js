4b5c1a6ebf00e5628b38badbaa342e36fc05f716
EvanLin018 <evan.lin@lincko.com>
2017-07-27 11:59:09 +0800

    20170727-make socket connect better

--- bundles/lincko/app/public/scripts/generic/linckoSocket.js
+++ bundles/lincko/app/public/scripts/generic/linckoSocket.js
@@ -11,63 +11,45 @@ linckoSocket.prototype.connect = function(){
 	{
 		return this.socket;
 	}
-	this.socket = new WebSocket(this.server_url);
-	this.socket.addEventListener('open', function (e){ 
-		var data = {
-			uid : wrapper_localstorage.uid,
-			sha : wrapper_localstorage.sha
-		};
-		that.socket.send(JSON.stringify(data));
-		that.timer ++ ;
-		storage_check_timing_speed = 1;
-	});
 
-	this.socket.addEventListener('message', function (e){ 
-		if(e.data != 'ping'){
-			try{
-				//wrapper_ajax_success(JSON.parse(e.data));
-				var response = JSON.parse(e.data);
-				//debugger;
-				var data = {};
-				data.partial = {};
-				data.partial[wrapper_localstorage.uid] = response.msg;
-				data.info = response.info;
-				storage_cb_success('', response.error, response.status, data);
-			}catch(e){}
-		}
-	});
-
-	this.socket.addEventListener('close', function(e){ 
-		storage_check_timing_speed = 3;
-		setTimeout(function(){
-			if(that.timmer <= 10)
-			{
-				that.socket = that.connect();
-			}
-		},500);
-	});
-	return this.socket;
-}
-
-linckoSocket.prototype.connection = function(){
-	return this.socket;
-}
-
-linckoSocket.prototype.close = function(){
-	this.socket.close();
-	this.socket = null;
-}
-
-var socket;
-JSfiles.finish(function(){
 	try{
-		socket = new linckoSocket();
-		if(socket != null)
-		{
-			socket.connect();
-			storage_check_timing_speed = 1;
-		}
+		this.socket = new WebSocket(this.server_url);
 
+		this.socket.addEventListener('open', function (e){ 
+			var data = {
+				uid : wrapper_localstorage.uid,
+				sha : wrapper_localstorage.sha
+			};
+			that.socket.send(JSON.stringify(data));
+			that.timer ++ ;
+			storage_check_timing_speed = 1;
+		});
+
+		this.socket.addEventListener('message', function (e){ 
+			if(e.data != 'ping'){
+				try{
+					//wrapper_ajax_success(JSON.parse(e.data));
+					var response = JSON.parse(e.data);
+					//debugger;
+					var data = {};
+					data.partial = {};
+					data.partial[wrapper_localstorage.uid] = response.msg;
+					data.info = response.info;
+					storage_cb_success('', response.error, response.status, data);
+				}catch(e){}
+			}
+		});
+
+		this.socket.addEventListener('close', function(e){ 
+			storage_check_timing_speed = 3;
+			setTimeout(function(){
+				if(that.timer <= 1000)
+				{
+					that.socket = that.connect();
+				}
+			},2000);
+		});
+		return this.socket;
 	}catch(e){
 		storage_check_timing_speed = 3;
 		var instance = "Other";
@@ -95,6 +77,28 @@ JSfiles.finish(function(){
 			stack = e.stack;
 		}
 		JSerror.sendError(stack, fileName+" "+message, lineNumber, columnNumber, instance+" "+name);
+		return null;
 	}
+
 	
+	
+}
+
+linckoSocket.prototype.connection = function(){
+	return this.socket;
+}
+
+linckoSocket.prototype.close = function(){
+	this.socket.close();
+	this.socket = null;
+}
+
+var socket;
+JSfiles.finish(function(){
+	socket = new linckoSocket();
+	if(socket != null)
+	{
+		socket.connect();
+		storage_check_timing_speed = 1;
+	}
 });
